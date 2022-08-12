cbbcfd8d6a2c46c68cb77e54beee362bcb66ed5c
fergus-earley <111060314+fergus-earley@users.noreply.github.com>
2022-08-12 10:25:21 +0100

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -2,15 +2,21 @@ def ledenable(bool2: bool):
     pass
 def displayenable(bool3: bool):
     pass
-displayenable(False)
-strip1 = neopixel.create(DigitalPin.P0, 12, NeoPixelMode.RGB)
-strip2 = neopixel.create(DigitalPin.P1, 12, NeoPixelMode.RGB)
+led.enable(False)
+strip1 = neopixel.create(DigitalPin.P4, 12, NeoPixelMode.RGB)
+strip2 = neopixel.create(DigitalPin.P6, 12, NeoPixelMode.RGB)
+strip3 = neopixel.create(DigitalPin.P7, 12, NeoPixelMode.RGB)
+strip4 = neopixel.create(DigitalPin.P9, 12, NeoPixelMode.RGB)
 # strip3 = neopixel.create(DigitalPin.P2, 12, NeoPixelMode.RGB)
 # strip4 = neopixel.create(DigitalPin.P3, 12, NeoPixelMode.RGB)
 while True:
-    if pins.digital_read_pin(DigitalPin.P4) == 1 and pins.digital_read_pin(DigitalPin.P6) == 1:
+    if pins.digital_read_pin(DigitalPin.P0) == 1 and pins.digital_read_pin(DigitalPin.P1) == 1 and (pins.digital_read_pin(DigitalPin.P2) == 1 and pins.digital_read_pin(DigitalPin.P3) == 1):
         strip1.show_color(neopixel.colors(NeoPixelColors.RED))
         strip2.show_color(neopixel.colors(NeoPixelColors.RED))
+        strip3.show_color(neopixel.colors(NeoPixelColors.RED))
+        strip4.show_color(neopixel.colors(NeoPixelColors.RED))
     else:
         strip1.show_color(neopixel.colors(NeoPixelColors.GREEN))
         strip2.show_color(neopixel.colors(NeoPixelColors.GREEN))
+        strip3.show_color(neopixel.colors(NeoPixelColors.GREEN))
+        strip4.show_color(neopixel.colors(NeoPixelColors.GREEN))
--- main.ts
+++ main.ts
@@ -1,27 +1,90 @@
-function ledenable (bool: boolean) {
-	
-}
-function displayenable (bool: boolean) {
-	
-}
-displayenable(false)
-ledenable(false)
-let strip1 = neopixel.create(DigitalPin.P3, 12, NeoPixelMode.RGB)
-let strip2 = neopixel.create(DigitalPin.P4, 12, NeoPixelMode.RGB)
-let strip3 = neopixel.create(DigitalPin.P6, 12, NeoPixelMode.RGB)
-let strip4 = neopixel.create(DigitalPin.P7, 12, NeoPixelMode.RGB)
+led.enable(false)
+let strip1 = neopixel.create(DigitalPin.P4, 12, NeoPixelMode.RGB)
+let strip2 = neopixel.create(DigitalPin.P6, 12, NeoPixelMode.RGB)
+let strip3 = neopixel.create(DigitalPin.P7, 12, NeoPixelMode.RGB)
+let strip4 = neopixel.create(DigitalPin.P9, 12, NeoPixelMode.RGB)
 // strip3 = neopixel.create(DigitalPin.P2, 12, NeoPixelMode.RGB)
 // strip4 = neopixel.create(DigitalPin.P3, 12, NeoPixelMode.RGB)
 while (true) {
-    if (pins.digitalReadPin(DigitalPin.P13) == 1 && pins.digitalReadPin(DigitalPin.P14) == 1 && (pins.digitalReadPin(DigitalPin.P15) == 1 && pins.digitalReadPin(DigitalPin.P16) == 1)) {
+    if (pins.digitalReadPin(DigitalPin.P0) == 1 && pins.digitalReadPin(DigitalPin.P1) == 1 && (pins.digitalReadPin(DigitalPin.P2) == 1 && pins.digitalReadPin(DigitalPin.P3) == 1)) {
         strip1.showColor(neopixel.colors(NeoPixelColors.Red))
         strip2.showColor(neopixel.colors(NeoPixelColors.Red))
         strip3.showColor(neopixel.colors(NeoPixelColors.Red))
         strip4.showColor(neopixel.colors(NeoPixelColors.Red))
-    } else if (false) {
-    	
+    } else if (pins.digitalReadPin(DigitalPin.P0) == 0 && pins.digitalReadPin(DigitalPin.P1) == 1 && (pins.digitalReadPin(DigitalPin.P2) == 1 && pins.digitalReadPin(DigitalPin.P3) == 1)) {
+        strip1.showColor(neopixel.colors(NeoPixelColors.Black))
+        strip2.showColor(neopixel.colors(NeoPixelColors.Red))
+        strip3.showColor(neopixel.colors(NeoPixelColors.Red))
+        strip4.showColor(neopixel.colors(NeoPixelColors.Red))
+    } else if (pins.digitalReadPin(DigitalPin.P0) == 1 && pins.digitalReadPin(DigitalPin.P1) == 0 && (pins.digitalReadPin(DigitalPin.P2) == 1 && pins.digitalReadPin(DigitalPin.P3) == 1)) {
+        strip1.showColor(neopixel.colors(NeoPixelColors.Red))
+        strip2.showColor(neopixel.colors(NeoPixelColors.Black))
+        strip3.showColor(neopixel.colors(NeoPixelColors.Red))
+        strip4.showColor(neopixel.colors(NeoPixelColors.Red))
+    } else if (pins.digitalReadPin(DigitalPin.P0) == 1 && pins.digitalReadPin(DigitalPin.P1) == 1 && (pins.digitalReadPin(DigitalPin.P2) == 0 && pins.digitalReadPin(DigitalPin.P3) == 1)) {
+        strip1.showColor(neopixel.colors(NeoPixelColors.Red))
+        strip2.showColor(neopixel.colors(NeoPixelColors.Red))
+        strip3.showColor(neopixel.colors(NeoPixelColors.Black))
+        strip4.showColor(neopixel.colors(NeoPixelColors.Red))
+    } else if (pins.digitalReadPin(DigitalPin.P0) == 1 && pins.digitalReadPin(DigitalPin.P1) == 1 && (pins.digitalReadPin(DigitalPin.P2) == 1 && pins.digitalReadPin(DigitalPin.P3) == 0)) {
+        strip1.showColor(neopixel.colors(NeoPixelColors.Red))
+        strip2.showColor(neopixel.colors(NeoPixelColors.Red))
+        strip3.showColor(neopixel.colors(NeoPixelColors.Red))
+        strip4.showColor(neopixel.colors(NeoPixelColors.Black))
+    } else if (pins.digitalReadPin(DigitalPin.P0) == 1 && pins.digitalReadPin(DigitalPin.P1) == 1 && (pins.digitalReadPin(DigitalPin.P2) == 0 && pins.digitalReadPin(DigitalPin.P3) == 0)) {
+        strip1.showColor(neopixel.colors(NeoPixelColors.Red))
+        strip2.showColor(neopixel.colors(NeoPixelColors.Red))
+        strip3.showColor(neopixel.colors(NeoPixelColors.Black))
+        strip4.showColor(neopixel.colors(NeoPixelColors.Black))
+    } else if (pins.digitalReadPin(DigitalPin.P0) == 1 && pins.digitalReadPin(DigitalPin.P1) == 0 && (pins.digitalReadPin(DigitalPin.P2) == 1 && pins.digitalReadPin(DigitalPin.P3) == 0)) {
+        strip1.showColor(neopixel.colors(NeoPixelColors.Red))
+        strip2.showColor(neopixel.colors(NeoPixelColors.Black))
+        strip3.showColor(neopixel.colors(NeoPixelColors.Red))
+        strip4.showColor(neopixel.colors(NeoPixelColors.Black))
+    } else if (pins.digitalReadPin(DigitalPin.P0) == 1 && pins.digitalReadPin(DigitalPin.P1) == 0 && (pins.digitalReadPin(DigitalPin.P2) == 0 && pins.digitalReadPin(DigitalPin.P3) == 1)) {
+        strip1.showColor(neopixel.colors(NeoPixelColors.Red))
+        strip2.showColor(neopixel.colors(NeoPixelColors.Black))
+        strip3.showColor(neopixel.colors(NeoPixelColors.Black))
+        strip4.showColor(neopixel.colors(NeoPixelColors.Red))
+    } else if (pins.digitalReadPin(DigitalPin.P0) == 0 && pins.digitalReadPin(DigitalPin.P1) == 0 && (pins.digitalReadPin(DigitalPin.P2) == 1 && pins.digitalReadPin(DigitalPin.P3) == 1)) {
+        strip1.showColor(neopixel.colors(NeoPixelColors.Black))
+        strip2.showColor(neopixel.colors(NeoPixelColors.Black))
+        strip3.showColor(neopixel.colors(NeoPixelColors.Red))
+        strip4.showColor(neopixel.colors(NeoPixelColors.Red))
+    } else if (pins.digitalReadPin(DigitalPin.P0) == 0 && pins.digitalReadPin(DigitalPin.P1) == 1 && (pins.digitalReadPin(DigitalPin.P2) == 0 && pins.digitalReadPin(DigitalPin.P3) == 1)) {
+        strip1.showColor(neopixel.colors(NeoPixelColors.Black))
+        strip2.showColor(neopixel.colors(NeoPixelColors.Red))
+        strip3.showColor(neopixel.colors(NeoPixelColors.Black))
+        strip4.showColor(neopixel.colors(NeoPixelColors.Red))
+    } else if (pins.digitalReadPin(DigitalPin.P0) == 0 && pins.digitalReadPin(DigitalPin.P1) == 1 && (pins.digitalReadPin(DigitalPin.P2) == 1 && pins.digitalReadPin(DigitalPin.P3) == 0)) {
+        strip1.showColor(neopixel.colors(NeoPixelColors.Black))
+        strip2.showColor(neopixel.colors(NeoPixelColors.Red))
+        strip3.showColor(neopixel.colors(NeoPixelColors.Red))
+        strip4.showColor(neopixel.colors(NeoPixelColors.Black))
+    } else if (pins.digitalReadPin(DigitalPin.P0) == 1 && pins.digitalReadPin(DigitalPin.P1) == 0 && (pins.digitalReadPin(DigitalPin.P2) == 0 && pins.digitalReadPin(DigitalPin.P3) == 0)) {
+        strip1.showColor(neopixel.colors(NeoPixelColors.Red))
+        strip2.showColor(neopixel.colors(NeoPixelColors.Black))
+        strip3.showColor(neopixel.colors(NeoPixelColors.Black))
+        strip4.showColor(neopixel.colors(NeoPixelColors.Black))
+    } else if (pins.digitalReadPin(DigitalPin.P0) == 0 && pins.digitalReadPin(DigitalPin.P1) == 1 && (pins.digitalReadPin(DigitalPin.P2) == 0 && pins.digitalReadPin(DigitalPin.P3) == 0)) {
+        strip1.showColor(neopixel.colors(NeoPixelColors.Black))
+        strip2.showColor(neopixel.colors(NeoPixelColors.Red))
+        strip3.showColor(neopixel.colors(NeoPixelColors.Black))
+        strip4.showColor(neopixel.colors(NeoPixelColors.Black))
+    } else if (pins.digitalReadPin(DigitalPin.P0) == 0 && pins.digitalReadPin(DigitalPin.P1) == 0 && (pins.digitalReadPin(DigitalPin.P2) == 1 && pins.digitalReadPin(DigitalPin.P3) == 0)) {
+        strip1.showColor(neopixel.colors(NeoPixelColors.Black))
+        strip2.showColor(neopixel.colors(NeoPixelColors.Black))
+        strip3.showColor(neopixel.colors(NeoPixelColors.Red))
+        strip4.showColor(neopixel.colors(NeoPixelColors.Black))
+    } else if (pins.digitalReadPin(DigitalPin.P0) == 0 && pins.digitalReadPin(DigitalPin.P1) == 0 && (pins.digitalReadPin(DigitalPin.P2) == 0 && pins.digitalReadPin(DigitalPin.P3) == 1)) {
+        strip1.showColor(neopixel.colors(NeoPixelColors.Black))
+        strip2.showColor(neopixel.colors(NeoPixelColors.Black))
+        strip3.showColor(neopixel.colors(NeoPixelColors.Black))
+        strip4.showColor(neopixel.colors(NeoPixelColors.Red))
     } else {
-        strip1.showColor(neopixel.colors(NeoPixelColors.Green))
-        strip2.showColor(neopixel.colors(NeoPixelColors.Green))
+        strip1.showColor(neopixel.colors(NeoPixelColors.Black))
+        strip2.showColor(neopixel.colors(NeoPixelColors.Black))
+        strip3.showColor(neopixel.colors(NeoPixelColors.Black))
+        strip4.showColor(neopixel.colors(NeoPixelColors.Black))
     }
 }
